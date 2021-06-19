c1d04e9d02501cfda198a33c6d3ace334dfe0672
Elizaveta Ozerova <e.ozerova@adsterra.com>
2021-06-19 15:27:31 +0300

    добавлен компонент с рендер-функцией и слотами

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
-  <h1>Hi, Michail! Let's play a memory game.</h1>
-  <p>Remember and open all blue cells</p>
+  <Heading v-bind:level="1">
+    <p>Hi, Michail!</p>
+    <p>Let's play a memory game.</p>
+  </Heading>
+  <Heading v-bind:level="2">
+    Remember and open all blue cells
+  </Heading>
 
   <Board />
 </template>
 
 <script>
-import Board from './components/Board';
+import Board from '@/components/Board';
+import Heading from '@/components/Heading';
 
 export default {
   name: 'App',
   components: {
     Board,
+    Heading,
   }
 }
 </script>
@@ -29,6 +36,16 @@ body {
   animation: gradient 15s ease infinite;
   background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
   background-size: 400% 400%;
+}
+
+h1 {
+  font-size: 25px;
+  line-height: 25px;
+}
+
+h2 {
+  font-size: 20px;
+  font-weight: normal;
 }
 
 @keyframes gradient {
---
+++ src/components/Heading.vue
@@ -0,0 +1,19 @@
+<script>
+import { h } from 'vue';
+
+export default {
+  name: 'Heading',
+  render: function () {
+    return h(
+      'h' + this.level,
+      this.$slots.default(),
+    )
+  },
+  props: {
+    level: {
+      type: Number,
+      required: true,
+    }
+  }
+}
+</script>
